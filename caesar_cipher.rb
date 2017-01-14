cd5cf759872f9ce6869083a3734ad1e9648f7d44
CyprianS <szwajka.cyprian@gmail.com>
2017-01-14 15:21:54 +0100

    Final

---
+++ caesar_cipher.rb
@@ -0,0 +1,21 @@
+def ciphering(message,key)
+	alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+	ciphered = ""
+	
+	message.each_char do |i|
+		if alphabet.include?(i)
+			number = (alphabet.index(i) + key) % 26
+			ciphered += alphabet[number]
+		elsif alphabet.include?(i.downcase)
+			number = (alphabet.index(i.downcase) + key) % 26
+			ciphered += alphabet[number].upcase
+		else
+			ciphered += i
+		end
+	end
+	ciphered
+end
+
+start_time = Time.now
+p ciphering("What a string!",5)
+p "Time elapsed: #{(Time.now - start_time)*1000} milliseconds"
